e5ec1faa32c44cd8569541c136df4336f080c902
rustamrv <rystmarv@gmail.com>
2020-07-27 20:00:52 +0300

    fixed

--- accounts/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Profile
-
-
-@admin.register(Profile)
-class ProdileAdmin(admin.ModelAdmin):
-    list_display = ('email', 'full_name', )
--- room/routing.py
+++
@@ -1,8 +0,0 @@
-from django.urls import re_path
-from django.conf.urls import url
-
-from .consumers import ChatConsumer
-
-websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<room_name>\w+)/$', ChatConsumer)
-]
